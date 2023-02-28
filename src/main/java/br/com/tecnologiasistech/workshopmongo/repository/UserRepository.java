a5e807a61958710d570206fa6f9f22e626c10731
Uériclis Lima <uericlislima@gmail.com>
2023-02-28 16:36:42 -0300

    Conectnado mongodb e services

---
+++ src/main/java/br/com/tecnologiasistech/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package br.com.tecnologiasistech.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import br.com.tecnologiasistech.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+	
+	
+	
+}
---
+++ src/main/java/br/com/tecnologiasistech/workshopmongo/services/UserService.java
@@ -0,0 +1,18 @@
+package br.com.tecnologiasistech.workshopmongo.services;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import br.com.tecnologiasistech.workshopmongo.domain.User;
+import br.com.tecnologiasistech.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+}
