04c52d197d7282858bcc6e39baa2bbcc88c7afc5
Uériclis Lima <uericlislima@gmail.com>
2023-03-01 15:24:29 -0300

    Projeto final

--- src/main/java/br/com/tecnologiasistech/workshopmongo/services/PostService.java
+++ src/main/java/br/com/tecnologiasistech/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package br.com.tecnologiasistech.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +29,12 @@ public class PostService {
 	public List<Post> findByTitle(String text) {
 		//return repo.findByTitleContainingIgnoreCase(text);
 		return repo.findByTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 *1000);
+		
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 	
 }
